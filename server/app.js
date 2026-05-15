48c6dcb9ccbd0949d367f73807353586edcba8b0
shivang meena <shivang340908@gmail.com>
2026-05-15 18:35:14 +0530

    fix issue

--- server/app.js
+++ server/app.js
@@ -8,11 +8,12 @@ const app=express();
 import route from "./routes/userRoutes.js";
 
 
-app.use(cors({
-    origin: ["http://localhost:5173",
-        "https://assesment-murex-rho.vercel.app"
-    ]
-}));
+// app.use(cors({
+//     origin: ["http://localhost:5173",
+//         "https://assesment-murex-rho.vercel.app"
+//     ]
+// }));
+app.use(cors());
 app.use(express.json());
 connectDB();
 
