243eb294188c232080567347f23e49361b944f1a
shivang meena <shivang340908@gmail.com>
2026-05-16 13:24:50 +0530

    switch to brevo smtp

--- server/utils/mailconfig.js
+++ server/utils/mailconfig.js
@@ -1,21 +1,13 @@
-import "dotenv/config"  
-import nodemailer from "nodemailer"
-// import dotenv from "dotenv"
+import nodemailer from "nodemailer";
 
-// dotenv.config();
-
-
-console.log("EMAIL:", process.env.EMAIL); // ✅ add this
-console.log("PASS:", process.env.PASS);   
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",  // ✅ explicit host
-    port: 587,               // ✅ use 587 instead of 465
-    secure: false,           // ✅ false for port 587
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
     auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASS
+        user: process.env.BREVO_USER,  
+        pass: process.env.BREVO_PASS 
     }
 });
-
 
 export default transporter;
